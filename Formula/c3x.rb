36ca89a9ab8a125af4745836081b23de4b3d1ba1
Andrea Cappelletti <andreacappelletti97@gmail.com>
2026-04-27 17:52:01 -0400

    Fix binary name in formula

--- Formula/c3x.rb
+++ Formula/c3x.rb
@@ -8,9 +8,17 @@ class C3x < Formula
     if Hardware::CPU.arm?
       url "https://github.com/c3xdev/c3x/releases/download/v#{version}/c3x-darwin-arm64.tar.gz"
       sha256 "581d55dcdbcaede278a64b6dab4a02368de43a9639b10d6423e3688546b8045e"
+
+      def install
+        bin.install "c3x-darwin-arm64" => "c3x"
+      end
     else
       url "https://github.com/c3xdev/c3x/releases/download/v#{version}/c3x-darwin-amd64.tar.gz"
       sha256 "eb92dd23e110089763c1e5fbc8da432af35275eb94f508b288aedc8142a34136"
+
+      def install
+        bin.install "c3x-darwin-amd64" => "c3x"
+      end
     end
   end
 
@@ -18,14 +26,18 @@ class C3x < Formula
     if Hardware::CPU.arm?
       url "https://github.com/c3xdev/c3x/releases/download/v#{version}/c3x-linux-arm64.tar.gz"
       sha256 "5f71afd61f1e298dbe338e5108bc4165a4e60eb3b8fa6d637dc406172705ff42"
+
+      def install
+        bin.install "c3x-linux-arm64" => "c3x"
+      end
     else
       url "https://github.com/c3xdev/c3x/releases/download/v#{version}/c3x-linux-amd64.tar.gz"
       sha256 "7b3c344ed651ea02efe4721c38a9028f2de9c0f0ea92e12b5a8cb38aaa4894df"
-    end
-  end
 
-  def install
-    bin.install "c3x"
+      def install
+        bin.install "c3x-linux-amd64" => "c3x"
+      end
+    end
   end
 
   test do
