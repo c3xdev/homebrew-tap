225a15d9a21bf5456aeae182f107afd56672e730
Andrea Cappelletti <andreacappelletti97@gmail.com>
2026-04-27 10:14:19 -0400

    Update c3x formula to v1.0.0 with real SHA256 hashes

--- Formula/c3x.rb
+++ Formula/c3x.rb
@@ -7,20 +7,20 @@ class C3x < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/c3xdev/c3x/releases/download/v#{version}/c3x-darwin-arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "581d55dcdbcaede278a64b6dab4a02368de43a9639b10d6423e3688546b8045e"
     else
       url "https://github.com/c3xdev/c3x/releases/download/v#{version}/c3x-darwin-amd64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "eb92dd23e110089763c1e5fbc8da432af35275eb94f508b288aedc8142a34136"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/c3xdev/c3x/releases/download/v#{version}/c3x-linux-arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "5f71afd61f1e298dbe338e5108bc4165a4e60eb3b8fa6d637dc406172705ff42"
     else
       url "https://github.com/c3xdev/c3x/releases/download/v#{version}/c3x-linux-amd64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "7b3c344ed651ea02efe4721c38a9028f2de9c0f0ea92e12b5a8cb38aaa4894df"
     end
   end
 
